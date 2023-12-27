484c82b0d1859db17476f4c0464aa3f0bebc998b
Razz67 <srasberry00@gmail.com>
2023-12-26 23:06:30 -0500

    Set up MySQL connection

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,8 +77,16 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 's_rasberry',
+        'PASSWORD':'Dattrazz67$$',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4', 
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
