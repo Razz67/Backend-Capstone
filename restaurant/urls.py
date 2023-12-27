be4dc6913230fef20635f7bbbbbb814556604780
Razz67 <srasberry00@gmail.com>
2023-12-26 23:56:20 -0500

    Set up Menu API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-item'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from restaurant.models import Menu, Booking  
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
